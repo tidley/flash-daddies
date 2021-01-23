a9a1fc25368a7aafcb3a17111393afd8d2bf6a72
Laurence Kirk <laurence@extropy.io>
2021-01-23 20:25:33 +0000

    Add initial unit tests

--- dev/blockchain/test/shame.js
+++ dev/blockchain/test/shame.js
@@ -11,11 +11,17 @@ before(async () => {
   accounts = await web3.eth.getAccounts();
   owner = accounts[0];
   bad = accounts[1];
-  shame_contract = Shame.deployed();
+  shame_contract = await Shame.deployed();
 });
 
 describe("shame tests", () => {
-  it("Deploys a contract", async () => {
+  it("Deploys the ERC721 contract", async () => {
     assert.ok(Shame.address);
+  });
+  it("mints a token to the bad address", async () => {
+    let bad_initial_balance = await shame_contract.balanceOf(bad);
+    await shame_contract.mint(bad, { from: owner });
+    let bad_final_balance = await shame_contract.balanceOf(bad);
+    assert(bad_initial_balance.toNumber() + 1 == bad_final_balance.toNumber());
   });
 });
