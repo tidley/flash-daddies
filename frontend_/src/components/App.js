34db5439fbe26c7e718fd7c5dda695a5d07650e2
Vincent <vincent@extropy.io>
2021-01-31 14:17:48 +0000

    fluff removal

--- frontend_/src/components/App.js
+++ frontend_/src/components/App.js
@@ -11,31 +11,15 @@ class App extends Component {
 		leaderboardList: [],
 	}
 
-	//bruh
+	
 
   constructor(props) {
     super(props)
-    // this.state = {
-    //   account: '',
-    //   leaderboard: '',
-    //   username: '',
-    //   score: 0,      
-    //   currentLevelAddress: '',
-    //   leaderboardLength: 0,
-    //   leaderboardList: [],
-    //   playerLevels: []
-	// }
 	
-
-    this.loadWeb3 = this.loadWeb3.bind(this)
+	this.loadWeb3 = this.loadWeb3.bind(this)
     // this.loadBlockchainData = this.loadBlockchainData.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    // this.setUsername = this.setUsername.bind(this)
-    // this.registerUserLevel = this.registerUserLevel.bind(this)
-    // this.submitSolution = this.submitSolution.bind(this)
-    // this.updateLeaderboard = this.updateLeaderboard.bind(this)
-    // this.getPlayerLevels = this.getPlayerLevels.bind(this)    
-    
+        
     
   }
 
@@ -60,18 +44,14 @@ class App extends Component {
 
   async loadBoguseaderBoard(){
 
+    this.setState({ score: 9999});
 
-	console.log("\n\nhello lads\n\n");
+    var ll = [];
 
-	this.setState({ score: 9999});
+    ll.push( {'username':"bob", 'address':"0x71C7656EC7ab88b098defB751B7401B5f6d8976F", 'score':222});
+    ll.push( {'username':"alice", 'address':"0x04148d6CE66cbb2F5c198c272B8117a5e7a6102E", 'score':10});
 
-	var ll = [];
-
-	ll.push( {'username':"bob", 'address':"0x71C7656EC7ab88b098defB751B7401B5f6d8976F", 'score':222});
-	ll.push( {'username':"alice", 'address':"0x04148d6CE66cbb2F5c198c272B8117a5e7a6102E", 'score':10});
-
-
-	this.setState({leaderboardList:ll });
+    this.setState({leaderboardList:ll });
 
   }
 
@@ -89,54 +69,6 @@ class App extends Component {
     }
   }
 
-//   async loadBlockchainData() {
-
-//     const web3 = window.web3
-//     // Load account
-//     const accounts = await web3.eth.getAccounts()
-//     this.setState({ account: accounts[0] })
-//     // Network ID
-//     const networkId = await web3.eth.net.getId()
-//     const networkData = Leaderboard.networks[networkId]
-
-//     if(networkData) {
-//       // Load contract
-//       const leaderboard = web3.eth.Contract(Leaderboard.abi, networkData.address)
-//       this.setState({ leaderboard })
-//       // Load username, if any
-//       if(this.state.username === '') {
-//         let username = await this.state.leaderboard.methods.getUsername(this.state.account).call()
-//         this.setState({ username })
-//       }
-
-//       if(this.state.score == 0) {
-//         let score = await this.state.leaderboard.methods.getScore(this.state.account).call()
-//         console.log(parseInt(score))
-//         this.setState({ score: parseInt(score) })
-//       }
-
-//       if(this.state.leaderboardList.length == 0){
-//         let leaderboardSize = await this.state.leaderboard.methods.listSize().call()
-//         leaderboardSize = parseInt(leaderboardSize)
-//         this.setState({leaderboardLength: leaderboardSize})
-//         let leaderboard = await this.state.leaderboard.methods.getTop(leaderboardSize).call()
-//         let ll = []
-//         for (let i in leaderboard) {
-//           let username = await this.state.leaderboard.methods.getUsername(leaderboard[i]).call()
-//           let score = await this.state.leaderboard.methods.getScore(leaderboard[i]).call()
-//           let player = {'address':leaderboard[i], 'username':username, 'score':parseInt(score)}
-//           ll.push(player)
-//         }
-//         this.setState({leaderboardList: ll})
-//       }
-
-//       this.getPlayerLevels()
-
-//     } else {
-//       window.alert('Leaderboard contract not deployed to detected network.')
-//     }
-//   }
-
   handleChange(evt) {
     const { name, value } = evt.target
     this.setState({
@@ -144,59 +76,15 @@ class App extends Component {
     })
   }
 
-
   // 
   async updateLeaderboard2() {
 
-
-
-
   }
-
-//   async updateLeaderboard() {
-//     // find the address pointing to this.state.account's new score and the address pointing to this.state.account's old score (if any)
-//     let leaderboardSize = await this.state.leaderboard.methods.listSize().call()
-//     leaderboardSize = parseInt(leaderboardSize)
-//     let oldPrevAddress // the address in the mapping that points to the current (prior updating leaderboard) score of the player
-//     let newPrevAddress = 'tmp' // the address in the mapping that points to the new (after updating leaderboard) score of the player
-//     let currentAddress = '0x0000000000000000000000000000000000000001' // This is the first address in the linked mapping list
-//     let nextAddress
-//     let nextScore
-    
-//     let playerScore = await this.state.leaderboard.methods.getScore(this.state.account).call()
-//     for(let i=0; i<=leaderboardSize; i++) {
-//         nextAddress = await this.state.leaderboard.methods.linkedLeaderboard(currentAddress).call()
-//         nextScore = await this.state.leaderboard.methods.getScore(nextAddress).call()
-//         if(nextScore <= playerScore && newPrevAddress == 'tmp') {
-//             newPrevAddress = currentAddress
-//         }
-//         // continuously set oldPrevAddress to currentAddress,
-//         // if the player is new, at the end of the loop this will equal the last player in the leaderboard (which points to GUARD)
-//         // if the player already exist, this loop will break before it gets updated
-//         oldPrevAddress = currentAddress;
-//         if(nextAddress == this.state.account) {
-//             break
-//         }
-//         if(i == leaderboardSize && newPrevAddress == 'tmp') {
-//             newPrevAddress = currentAddress
-//         }
-//         currentAddress = nextAddress
-//     }
-//     console.log(newPrevAddress, oldPrevAddress)
-//     if(newPrevAddress=='tmp'){
-//       // i.e. if this is the very first player, hence the leaderboard is empty
-//       await this.state.leaderboard.methods.updateLeaderboard('0x0000000000000000000000000000000000000001', '0x0000000000000000000000000000000000000001').send({ from: this.state.account })
-//     } else {
-//       await this.state.leaderboard.methods.updateLeaderboard(newPrevAddress, oldPrevAddress).send({ from: this.state.account })
-//     }
-//   }
 
 
   render() {
     return (
-      <div>
-
-		  hellofrgdrgdrgrd
+      <div>		  
         <Navi
           account={this.state.account}
           username={this.state.username}
--- frontend_/src/components/Content.js
+++ frontend_/src/components/Content.js
@@ -5,15 +5,7 @@ import {
   HashRouter
 } from "react-router-dom";
 import Home from "./content/Home";
-// import Level_0_Practice from "./content/Level_0_Practice";
-// import Level_1_Brute_Force from "./content/Level_1_Brute_Force";
-// import Level_2_Reentrancy from "./content/Level_2_Reentrancy";
-// import Level_3_Global_Functions from "./content/Level_3_Global_Functions";
-// import DemoLevel3 from "./content/DemoLevel3";
-// import Level1 from "./content/Level1";
-// import Contact from "./content/Contact";
 import Leaderboard from "./content/Leaderboard";
-
  
 function Content(props) {
     return (
@@ -21,11 +13,7 @@ function Content(props) {
         <div className="row mt-5">
     		<div className="col-sm-2">
         	<ul className="navbar-nav px-3">
-            {/*<li>
-              <NavLink exact to="/" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Home
-              </NavLink>
-            </li>*/}
+  
             <li>
               <NavLink exact to="/home" activeStyle={{ fontWeight: "bold", color: "green" }} >
                 Home
@@ -37,49 +25,7 @@ function Content(props) {
                 Leaderboard
               </NavLink>
             </li>
-            {/* <li>
-              <NavLink exact to="/profile" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                User Profile
-              </NavLink>
-            </li>
-            <br></br>
-            LEVELS
-            <br></br>
-            <li>
-              <NavLink exact to="/Level_0_Practice" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Level 0 - Practice
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/Level_1_Brute_Force" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Level 1 - Brute Force
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/Level_2_Reentrancy" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Level 2 - Reentrancy
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/Level_3_Global_Functions" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Level 3 - Global Functions
-              </NavLink>
-            </li> */}
-            {/*<li>
-              <NavLink exact to="/demolevel3" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Demo Level 3
-              </NavLink>
-            </li>*/}
-            {/*<li>
-              <NavLink exact to="/level1" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Lottery
-              </NavLink>
-            </li>*/}
-            {/*<li>
-              <NavLink exact to="/contact" activeStyle={{ fontWeight: "bold", color: "green" }} >
-                Contact
-              </NavLink>
-            </li>*/}
+  
           </ul>
 	      </div>
         <div className="col-sm-10 pr-sm-5">			
@@ -96,70 +42,7 @@ function Content(props) {
               />
             }
             />
-          {/* <Route exact path="/profile"
-            render={()=>
-              <Profile
-                account={props.account}
-                username={props.username}
-                score={props.score}
-                setUsername={props.setUsername}
-                handleChange={props.handleChange}
-                leaderboardList={props.leaderboardList}
-                playerLevels={props.playerLevels}
-              />
-            }
-          />  */}
-          {/* <Route exact path="/Level_0_Practice"
-            render={()=>
-              <Level_0_Practice
-                account={props.account}
-                username={props.username}
-                handleChange={props.handleChange}
-                registerUserLevel={props.registerUserLevel}
-                submitSolution={props.submitSolution}
-                updateLeaderboard={props.updateLeaderboard}
-              />
-            }
-          />
-          <Route exact path="/Level_1_Brute_Force" 
-            render={()=>
-              <Level_1_Brute_Force
-                account={props.account}
-                username={props.username}
-                handleChange={props.handleChange}
-                registerUserLevel={props.registerUserLevel}
-                submitSolution={props.submitSolution}
-                updateLeaderboard={props.updateLeaderboard}
-              />
-            }
-          />
-          <Route exact path="/Level_2_Reentrancy" 
-            render={()=>
-              <Level_2_Reentrancy
-                account={props.account}
-                username={props.username}
-                handleChange={props.handleChange}
-                registerUserLevel={props.registerUserLevel}
-                submitSolution={props.submitSolution}
-                updateLeaderboard={props.updateLeaderboard}
-              />
-            }
-          />
-          <Route exact path="/Level_3_Global_Functions" 
-            render={()=>
-              <Level_3_Global_Functions
-                account={props.account}
-                username={props.username}
-                handleChange={props.handleChange}
-                registerUserLevel={props.registerUserLevel}
-                submitSolution={props.submitSolution}
-                updateLeaderboard={props.updateLeaderboard}
-              />
-            }
-          /> */}
-          {/*<Route exact path="/demolevel3" component={DemoLevel3}/>*/}
-          {/*<Route exact path="/level1" component={Level1}/>
-          <Route exact path="/contact" component={Contact}/>*/}
+          
         </div>
         </div>
       </HashRouter>
