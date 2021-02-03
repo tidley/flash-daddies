991b9e84943a4db0beee29aab645291f596b178a
Tom Dwyer <tom.dwyer@extropy.io>
2021-02-03 20:59:47 +0000

    Sending tx... ??

--- code/nodes/infura_endpoint/README.md
+++ code/nodes/infura_endpoint/README.md
@@ -1,1 +1,3 @@
 ganache-cli -p 7545 --fork INFURAENDPOINT --unlock 0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8
+
+Restart geth https://ethereum.stackexchange.com/questions/7346/restart-geth-from-another-process
--- code/nodes/infura_endpoint/main.js
+++ code/nodes/infura_endpoint/main.js
@@ -83,14 +83,42 @@ async function getAbiRemote(to) {
 async function sendTx(to, abi, method, inputs) {
   const contract = await new web3Local.eth.Contract(abi, to);
 
+  let input_1 = inputs[0];
+  let input_2 = inputs[1];
+  let input_3 = [
+    web3.utils
+      .toBN(
+        // 32323630666163356535353432613737336161343466626366656466376331393362633263353939,
+        String(inputs[2][0]),
+      )
+      .pow(100),
+    web3.utils
+      .toBN(
+        //        63303261616133396232323366653864306130653563346632376561643930383363373536636332,
+        String(inputs[2][1]),
+      )
+      .pow(100),
+  ];
+  // input_3.push(inputs[2][0]);
+  // input_3.push(inputs[2][1]);
+  // input_3 = web3.utils.toBN(input_3).pow(100);
+  let input_4 = inputs[3];
+  let input_5 = inputs[4].words;
   console.log('contract is fine');
 
   let response = await contract.methods[method](
-    String(inputs[0]),
-    String(inputs[1]),
-    String([inputs[2][0].toString(), inputs[2][1].toString()]),
-    String(inputs[3].toString()),
-    String(inputs[4].toString()),
+    input_1,
+    input_2,
+    input_3,
+    input_4,
+    input_5,
+
+    // String(inputs[0]),
+    // String(inputs[1]),
+    // // bn(String(10000000)).times(String(wei));
+    // [inputs[2][0], inputs[2][1]],
+    // String(inputs[3].toString()),
+    // String(inputs[4].toString()),
   ).send({ from: UNLOCK });
 
   console.log(response);
